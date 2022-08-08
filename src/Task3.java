9728bd5a4d13fdf69d0855dad6060108a109fc37
D-Mandal <debolinaddm@gmail.com>
2022-08-08 16:59:17 +0530

    task3

---
+++ src/Task3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char first_char=sc.next().charAt(0);
+        char second_char=sc.next().charAt(0);
+        System.out.println("Enter no of Rows : ");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int k=0;k<=i;k++){
+                System.out.print(first_char);
+            }
+            for(int j=n-1;j>=i;j--){
+                System.out.print(second_char);
+            }
+            System.out.println();
+        }
+    }
+}
