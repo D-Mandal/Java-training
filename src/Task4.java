ac52291e509a73f3a83ccdb3d181d1743e3029fa
D-Mandal <debolinaddm@gmail.com>
2022-08-08 18:44:09 +0530

    task 4

---
+++ src/Task4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Task4 {
+    public static void main(String args[])
+    {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter the no :");
+        int a= Sc.nextInt();
+
+        while (a != 1)
+        {
+            System.out.println(a + " ");
+
+
+            if ((a & 1) == 1)
+                a = 3 * a + 1;
+
+
+            else
+                a = a / 2;
+        }
+
+        // Print 1 at the end
+        System.out.print(a);
+    }
+    }
+
