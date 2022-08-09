7754d51b2cc2a039ae44fda8d12a23d2ad64cad7
D-Mandal <debolinaddm@gmail.com>
2022-08-09 12:35:00 +0530

    DAY 2 TASK 2

---
+++ src/Day2_Task2.java
@@ -0,0 +1,29 @@
+public class Day2_Task2 {
+    public static void main(String[] args)
+    {
+     String s1="Debolina"  ;
+     String s[] =s1.split("o",4);
+     for(String i:s){
+         System.out.println(i);
+     }
+     String s2="Mandal";
+     StringBuffer sb=new StringBuffer(s1);
+
+
+     System.out.println("The length of the string " +s1.length());
+     System.out.println("The length of the string " + s1+s2);
+
+     System.out.print("The reverse of string " +sb.reverse()+"\n");
+
+
+        if(s1.equalsIgnoreCase(s2)){
+            System.out.println("equal");
+        }
+        else{
+            System.out.println("not equal");
+        }
+
+    }
+
+    }
+
